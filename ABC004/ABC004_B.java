f88e610295c5166088d73ff6d8e1beb2d310b48b
kosei.aoyagi <k.aoyagi@tcmobile.jp>
2019-08-13 12:25:30 +0900

    004_B追加

--- ABC004/ABC004_B.java
+++ ABC004/ABC004_B.java
@@ -5,24 +5,25 @@ import java.util.Arrays;
 class ABC004_B {
     public static void main(String[] args) throws java.lang.Exception {
  
-        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String hoge = "o o x x";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[][] c = new String[4][4];
 
         for(int i = 0; i < 4; i++) {
-            String[] array = hoge.split(" ");
+            String[] array = br.readLine().split(" ");
             for(int j = 0; j < 4; j++) {
                 c[i][j] = array[j];
             }
         }
 
+        String[][] rotation = new String[4][4];
+
         for(int k = 0; k < 4; k++) {
             for(int l = 0; l < 4; l++) {
-                c[k][l] = c[3 - k][3 - l];
-                System.out.print(c[k][l] + " ");
+                rotation[k][l] = c[3 - k][3 - l];
+                System.out.print(rotation[k][l] + " ");
             }
             System.out.println("");
         }
-        
+
     }
 }
