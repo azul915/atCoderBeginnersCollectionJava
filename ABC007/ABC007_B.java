8562fc30e61442dd99d6c339c8f959e69f5abe95
azul915 <k.aoyagi940516@gmail.com>
2019-08-16 02:48:10 +0900

    007_B追加

---
+++ ABC007/ABC007_B.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class ABC007_B {
+    public static void main(String[] args) throws java.lang.Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] A = Arrays.stream(br.readLine().split("")).toArray(String[]::new);
+
+        System.out.println(A.length == 1 && A[0].equals("a") ? -1 : "a");
+
+    }
+}
