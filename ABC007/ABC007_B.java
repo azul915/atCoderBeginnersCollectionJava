746cd861226dec01df71dd3095a8ab27bce15faf
azul915 <k.aoyagi940516@gmail.com>
2019-08-16 02:48:22 +0900

    修正

--- ABC007/ABC007_B.java
+++ ABC007/ABC007_B.java
@@ -6,9 +6,7 @@ class ABC007_B {
     public static void main(String[] args) throws java.lang.Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String[] A = Arrays.stream(br.readLine().split("")).toArray(String[]::new);
-
         System.out.println(A.length == 1 && A[0].equals("a") ? -1 : "a");
 
     }
