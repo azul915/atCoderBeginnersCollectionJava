4b8f3a8a8efc411a3600707fd8d7e688cf8d7d5c
azul915 <k.aoyagi940516@gmail.com>
2019-12-29 13:18:30 +0900

    Add pattern ABC085_C

--- ABC085/ABC085_C.java
+++ ABC085/ABC085_C.java
@@ -23,3 +23,30 @@ class ABC085_C {
     }
 
 }
+
+// import java.io.BufferedReader;
+// import java.io.InputStreamReader;
+// import java.util.Arrays;
+// import java.util.stream.IntStream;
+
+// class Main {
+//   public static void main(String[] args) throws java.lang.Exception {
+
+//     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//     int[] intArray = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+//     int n = intArray[0];
+//     int y = intArray[1];
+
+//     IntStream.rangeClosed(0, n).forEach(ichiman -> {
+//       IntStream.rangeClosed(0, n -ichiman).forEach(gosen -> {
+//         int senen = n -ichiman -gosen;
+//         if(10000 *ichiman + 5000 *gosen + 1000 *senen == y) {
+//           System.out.println(ichiman +" " +gosen +" " +senen);
+//           System.exit(0);
+//         }
+//       });
+//     });
+
+//     System.out.println("-1 -1 -1");
+//   }
+// }
